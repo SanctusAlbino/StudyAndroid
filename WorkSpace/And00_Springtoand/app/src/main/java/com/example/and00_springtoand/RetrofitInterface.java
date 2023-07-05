988d0891976c97aeaa059c90917156a68b1b9ea6
SanctusAlbino <ghk1998@naver.com>
2023-07-05 18:16:22 +0900

    7.05
    ㄴㅇㄹㄴ

---
+++ WorkSpace/And00_Springtoand/app/src/main/java/com/example/and00_springtoand/RetrofitInterface.java
@@ -0,0 +1,22 @@
+package com.example.and00_springtoand;
+
+
+
+//public interface GitHubService {
+//    @GET("users/{user}/repos")
+//    Call<List<Repo>> listRepos(@Path("user") String user);
+//}
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface RetrofitInterface {
+    //Retrofit으로 return받는 모든 것들은 call<클래스타입> 형태를 띄고 있다. (규칙)
+
+    @GET("list.cu")
+    Call<String> getCuList();
+
+    @GET("obj.cu")
+    Call<String> getCu();
+
+}
